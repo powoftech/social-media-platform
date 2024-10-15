7eac88198ab1f17db3e42c81056971c40380a7f3
Phuong Dang <powoftech@gmail.com>
2024-10-16 03:02:03 +0700

    Refactor MyNetworkLayout component

--- app/mynetwork/layout.tsx
+++ app/mynetwork/layout.tsx
@@ -1,25 +1,26 @@
-"use client";
-import React, { useState } from "react";
-import Header from "../components/header";
+// "use client";
+// import React, { useState } from "react";
+// import Header from "../components/header";
 
-interface MyNetworkLayoutProps {
-  children: React.ReactNode;
-}
+// interface MyNetworkLayoutProps {
+//   children: React.ReactNode;
+// }
 
-const MyNetworkLayout = ({ children }: MyNetworkLayoutProps) => {
-  const [isSearchFocus, setIsSearchFocus] = useState(false);
-  return (
-    <div className="h-full">
-      {isSearchFocus && (
-        <div className="fixed inset-0 bg-black bg-opacity-30 z-10" />
-      )}
-      <Header
-        isSearchFocus={isSearchFocus}
-        setIsSearchFocus={setIsSearchFocus}
-      />
-      {children}
-    </div>
-  );
-};
 
-export default MyNetworkLayout;
+// const MyNetworkLayout = ({ children }: MyNetworkLayoutProps) => {
+//   const [isSearchFocus, setIsSearchFocus] = useState(false);
+//   return (
+//     <div className="h-full">
+//       {isSearchFocus && (
+//         <div className="fixed inset-0 bg-black bg-opacity-30 z-10" />
+//       )}
+//       <Header
+//         isSearchFocus={isSearchFocus}
+//         setIsSearchFocus={setIsSearchFocus}
+//       />
+//       {children}
+//     </div>
+//   );
+// };
+
+// export default MyNetworkLayout;
